50b6be4b5569a6f765355f0f42509b7567f68fe6
Richard Kotermanski <rek58@pitt.edu>
2016-02-16 15:15:52 -0500

    Matched rooms and attempted to match furniture where valid with original game.

--- Deliverable2/src/CoffeeMakerQuest.java
+++ Deliverable2/src/CoffeeMakerQuest.java
@@ -30,7 +30,7 @@ public class CoffeeMakerQuest {
 		// Main Game loop that finishes when game goes into
 		// 		a winning or losing state
 		while (g.getStatus() == GameStatus.IN_PROGRESS) {
-			System.out.print(">> ");
+			System.out.print("INSTRUCTIONS (N,S,L,I,D) >> ");
 			String d = s.nextLine();
 			System.out.println(g.progress(d));
 		}
@@ -44,9 +44,9 @@ public class CoffeeMakerQuest {
 		Map m = new Map();
 		// Set of unique roomAdjectives (room descriptions), furniture names, and furniture 
 		// 	adjectives that are associated with one another by index.
-		String[] rmAdj = { "Small", "Funny", "Rigid", "Big", "Northern" };
-		String[] furNm = { "desk", "armoire", "bed", "couch", "futon" };
-		String[] furAdj = { "decrepit", "Armenian", "bare", "comfy", "futuristic" };
+		String[] rmAdj = { "Small", "Funny", "Refinanced", "Dumb", "Bloodthirsty", "Rough" };
+		String[] furNm = { "sofa", "record player", "bed", "couch", "futon", "air hockey table" };
+		String[] furAdj = { "Quaint", "Sad", "Bare", "Comfy", "Futuristic", "Perfect" };
 		
 		// Item locations by index: 
 		//	{ coffee location index, cream location index, sugar location index }
