be0723183bb6a152b5c5c58787f8e7e0617ac3b4
Richard Kotermanski <rek58@pitt.edu>
2016-02-16 09:35:05 -0500

    Unfixed Comment indenting

--- Deliverable2/src/Player.java
+++ Deliverable2/src/Player.java
@@ -2,19 +2,19 @@
  * Richard Kotermanski 
  * CS1632 Deliverable 2 
  * Player - The entity of the quest
- * 		that has an inventory, acquires items for said inventory, and attempts to
- * 		drink from said inventory
+ * 			that has an inventory, acquires items for said inventory, and attempts to
+ * 			drink from said inventory
  */
 
 public class Player {
-	private boolean hasCream; // inventory state for whether
-				  // 	or not the player has acquired cream
+	private boolean hasCream; 	// inventory state for whether
+								// 	or not the player has acquired cream
 	
-	private boolean hasSugar; // inventory state for whether
-				  // 	or not the player has acquired sugar
+	private boolean hasSugar;	// inventory state for whether
+								// 	or not the player has acquired sugar
 	
-	private boolean hasCoffee; // inventory state for whether
-				   // 	or not the player has acquired coffee
+	private boolean hasCoffee; 	// inventory state for whether
+								// 	or not the player has acquired coffee
 
 	public Player() {
 		hasCream = false;
@@ -24,8 +24,8 @@ public class Player {
 	
 	
 	// Given that the inventory state for each item type (cream, sugar, and coffee)
-	//	is true and that the player has acquired them, then return true.
-	//	 Otherwise, it returns false.
+	//		is true and that the player has acquired them, then return true.
+	//		 Otherwise, it returns false.
 	public boolean drink() {
 		if (hasCoffee && hasCream && hasSugar) {
 			return true;
@@ -35,8 +35,8 @@ public class Player {
 
 	
 	// Creates and returns a boolean array where the indexes describe the inventory
-	//	state for each item in the format: 
-	//	{ coffee's inventory state, cream's inventory state, sugar's inventory state }
+	//		state for each item in the format: 
+	//		{ coffee's inventory state, cream's inventory state, sugar's inventory state }
 	public boolean[] getInventory() {
 		boolean[] it = { hasCoffee, hasCream, hasSugar };
 		return it;
@@ -44,10 +44,10 @@ public class Player {
 	
 	
 	// Given an item type, itm, the player will acquire the item
-	//	by setting the inventory state for the given item type
-	//	value to true. If the type, itm, is of type COFFEE,
-	//	CREAM, or SUGAR, then the function will return true;
-	//	otherwise, it will return false.
+	//		by setting the inventory state for the given item type
+	//		value to true. If the type, itm, is of type COFFEE,
+	//		CREAM, or SUGAR, then the function will return true;
+	//		otherwise, it will return false.
 	public boolean acquireItem(ItemType itm) {
 		if (itm == null)
 			return false;
